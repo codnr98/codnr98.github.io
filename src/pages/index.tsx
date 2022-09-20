2c787416b6aba0b90fe3478545b37d321e4f602a
codnr <wnd75913@gmail.com>
2022-09-20 14:26:24 +0900

    Comment: info.tsx 주석추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ const IndexPage: FunctionComponent = function () {
     <div>
       <Text text="Hello, World" />
       <Link to="/info/">To Info</Link>
+      {/* Gatsby에서 제공되는 Link 태그를 사용했고 경로를 to라는 이름의 props로 전달했다. */}
     </div>
   )
 }
--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -28,6 +28,7 @@ const InfoPage: FunctionComponent<InfoPageProps> = function ({
       <Text text={description} />
       <Text text={author} />
       <Link to="/">To Main</Link>
+      {/* Gatsby에서 제공되는 Link 태그를 사용했고 경로를 to라는 이름의 props로 전달했다. */}
     </div>
   )
 }
