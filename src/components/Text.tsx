60786b4b6fd1a9812b2873e91e4af4f58113f32a
codnr <wnd75913@gmail.com>
2022-09-20 14:18:34 +0900

    Comment: Text.tsx FunctionComponent설명 comment추가

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,6 +1,9 @@
 import React, { FunctionComponent } from 'react'
-type TextProps = { text: string }
-
+// FunctionComponent는 react에서 기본으로 제공해주는 함수형 컴포넌트 타입이다.
+interface TextProps {
+  text: string
+}
+//FunctionComponent에 TextProps Generic을 추가해서 컴포넌트가 받는 props의 타입을 지정할 수 있다.
 const Text: FunctionComponent<TextProps> = function ({ text }) {
   return <div>{text}</div>
 }
