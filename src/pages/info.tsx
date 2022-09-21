5a2fd575d6cf338e13603b9c67a618379bd1a9cd
codnr <wnd75913@gmail.com>
2022-09-21 10:31:39 +0900

    Add emotionJS

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -29,6 +29,7 @@ const InfoPage: FunctionComponent<InfoPageProps> = function ({
       <Text text={author} />
       <Link to="/">To Main</Link>
       {/* Gatsby에서 제공되는 Link 태그를 사용했고 경로를 to라는 이름의 props로 전달했다. */}
+      {/* 페이지가 로드되면 Gatsby는 리소스 로드 속도를 높이기 위해 현재 페이지에서 사용되는 모든 링크를 찾은 후, 각 링크의 페이지를 미리 로드한다. */}
     </div>
   )
 }
