da3f5aefc319d377d4ba25ca72ec99c56957b6cc
codnr <wnd75913@gmail.com>
2022-09-22 11:51:47 +0900

    Chore: index.tsx Link태그 제거

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import React, { FunctionComponent } from 'react'
 import Text from 'components/Text'
-import { Link } from 'gatsby'
 
 const IndexPage: FunctionComponent = function () {
   return (
     <div>
       <Text text="Hello, World" />
-      <Link to="/info/">To Info</Link>
-      {/* Gatsby에서 제공되는 Link 태그를 사용했고 경로를 to라는 이름의 props로 전달했다. */}
     </div>
   )
 }
