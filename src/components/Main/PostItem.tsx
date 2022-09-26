8bb8328035db5d4063debde55466c50309839279
codnr <wnd75913@gmail.com>
2022-09-26 14:15:38 +0900

    Feat: PostList 추가

---
+++ src/components/Main/PostItem.tsx
@@ -0,0 +1,114 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+import { Link } from 'gatsby'
+// 위 세가지 종류의 import 차이점?
+
+// 타입지정
+type PostItemProps = {
+  title: string
+  date: string
+  categories: string[]
+  summary: string
+  thumbnail: string
+  link: string
+}
+
+// Link 컴포넌트로 정의
+const PostItemWrapper = styled(Link)`
+  display: flex;
+  flex-direction: column;
+  border-radius: 10px;
+  box-shadow: 0 0 8px rgba(0, 0, 0, 0.15);
+  transition: 0.3s box-shadow;
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  }
+`
+//img 컴포넌트로 정의
+const ThumbnailImage = styled.img`
+  width: 100%;
+  height: 160px;
+  border-radius: 10px 10px 0 0;
+  object-fit: cover;
+`
+// div 컴포넌트로 정의
+const PostItemContent = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 15px;
+`
+const Title = styled.div`
+  display: -webkit-box;
+  overflow: hidden
+  margin-bottom: 3px
+  text-overflow: ellipsis
+  white-space: normal
+  overflow-wrap: break-word;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  font-size: 20px;
+  font-weight: 700;
+  `
+const Date = styled.div`
+  font-size: 14px;
+  font-weight: 400;
+  opacity: 0.7;
+`
+const Category = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 10px;
+  margin: 10px -5px;
+`
+const CategoryItem = styled.div`
+  margin: 2.5px 5px;
+  padding: 3px 5px;
+  border-radius: 3px;
+  background: black;
+  font-size: 14px;
+  font-weight: 700;
+  color: white;
+`
+const Summary = styled.div`
+  display: -webkit-box;
+  overflow: hidden;
+  margin-top: auto;
+  text-overflow: ellipsis;
+  white-space: normal;
+  overflow-wrap: break-word;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+  font-size: 16px;
+  opacity: 0.8;
+`
+const PostItem: FunctionComponent<PostItemProps> = function ({
+  title,
+  date,
+  categories,
+  summary,
+  thumbnail,
+  link,
+}) {
+  return (
+    <PostItemWrapper to={link}>
+      <ThumbnailImage src={thumbnail} alt="Post Item Image" />
+
+      <PostItemContent>
+        <Title>{title}</Title>
+        <Date>{date}</Date>
+        <Category>
+          {categories.map(category => (
+            <CategoryItem key={category}>{category}</CategoryItem>
+          ))}
+          {/* categories에서 배열을 받아 item들을 CategoryItem컴포넌트에 담아 새로운 배열을 만든다. */}
+        </Category>
+        <Summary>{summary}</Summary>
+      </PostItemContent>
+    </PostItemWrapper>
+  )
+}
+
+export default PostItem
--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -1,5 +1,17 @@
 import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
+import PostItem from './PostItem'
+
+const POST_ITEM_DATA = {
+  title: 'Post Item Title',
+  date: '2020.01.29.',
+  categories: ['Web', 'Frontend', 'Testing'],
+  summary:
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
+  thumbnail:
+    'https://cdn.inflearn.com/public/courses/326897/cover/9ee11890-b271-4bcd-bfda-ed7cb3bd2e1d/Blog%20Development.png',
+  link: 'https://www.google.co.kr/',
+}
 
 const PostListWrapper = styled.div`
   display: grid;
@@ -10,7 +22,14 @@ const PostListWrapper = styled.div`
   padding: 50px 0 100px;
 `
 const PostList: FunctionComponent = function () {
-  return <PostListWrapper></PostListWrapper>
+  return (
+    <PostListWrapper>
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+      <PostItem {...POST_ITEM_DATA} />
+    </PostListWrapper>
+  )
 }
-
+// 왜? ...를 써야할까?
 export default PostList
