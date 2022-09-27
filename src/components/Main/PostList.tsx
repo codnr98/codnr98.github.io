10e014cf02db0d81051d251d0340409b7dae5c68
codnr <wnd75913@gmail.com>
2022-09-27 13:39:51 +0900

    Chore: PostList.tsx 더미데이터 삭제

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -2,17 +2,6 @@ import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
 import PostItem from './PostItem'
 
-const POST_ITEM_DATA = {
-  title: 'Post Item Title',
-  date: '2020.01.29.',
-  categories: ['Web', 'Frontend', 'Testing'],
-  summary:
-    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident repellat doloremque fugit quis rem temporibus! Maxime molestias, suntrem debitis odit harum impedit. Modi cupiditate harum dignissimos eos in corrupti!',
-  thumbnail:
-    'https://cdn.inflearn.com/public/courses/326897/cover/9ee11890-b271-4bcd-bfda-ed7cb3bd2e1d/Blog%20Development.png',
-  link: 'https://www.google.co.kr/',
-}
-
 const PostListWrapper = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
@@ -28,14 +17,7 @@ const PostListWrapper = styled.div`
   }
 `
 const PostList: FunctionComponent = function () {
-  return (
-    <PostListWrapper>
-      <PostItem {...POST_ITEM_DATA} />
-      <PostItem {...POST_ITEM_DATA} />
-      <PostItem {...POST_ITEM_DATA} />
-      <PostItem {...POST_ITEM_DATA} />
-    </PostListWrapper>
-  )
+  return <PostListWrapper></PostListWrapper>
 }
 // 왜? ...를 써야할까?
 // 이유 일단 컴포넌트 Props로 배열이 오면 안되서 인듯
